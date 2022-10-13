b4ae1e6ec6335ec6a5074e76ed7463aa775adef3
AlekseiPanov <panov.a.a60@gmail.com>
2022-10-13 21:53:16 +0300

    1 пример

--- sem1/primer1/Program.cs
+++ sem1/primer1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// Задача 1: введем 2 числа на проверку числа квадратом другого
+ Console.Write("Введите первое число: ");
+ int number1 = int.Parse(Console.ReadLine()!);
+ Console.Write("Введите второе число: ");
+ int number2 = int.Parse(Console.ReadLine()!);
+ if ((number2*number2) == number1)
+    Console.WriteLine($"Квадрат числа {number2} равен {number1}, значит ДА!");
+else
+    Console.WriteLine($"Квадрат числа {number2} неравен {number1}, значит НЕТ!");
