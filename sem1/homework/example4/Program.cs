b668256a185c103b1a45077b10f1ab4be4a3a433
AlekseiPanov <panov.a.a60@gmail.com>
2022-10-14 23:04:23 +0300

    4 задача

---
+++ sem1/homework/example4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int beginCount = 2;
+while(beginCount <= number)
+{
+    Console.Write($"{beginCount} ");
+    beginCount += 2;
+}
