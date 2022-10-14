65ef08b6dcb0194801002879fb5f2dfae72dc82b
AlekseiPanov <panov.a.a60@gmail.com>
2022-10-14 19:41:43 +0300

    задание с проверкой

--- sem1/homework/example1/Program.cs
+++ sem1/homework/example1/Program.cs
@@ -4,10 +4,12 @@ int number1 = int.Parse(Console.ReadLine()!);
 
 Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine()!);
-
 if (number1 > number2)
 {
-Console.WriteLine($"Число {number1} ,больше");
+    Console.WriteLine($"Число {number1} больше");
 }
-else 
-    Console.WriteLine($"Число {number2} ,больше");
+else
+    if(number1 == number2)
+          Console.WriteLine($"Числа равны");
+else
+    Console.WriteLine($"Число {number2} больше");
