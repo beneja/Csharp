e909dee9b5301a573f70d17fc185e3f0d584fb9f
AlekseiPanov <panov.a.a60@gmail.com>
2022-10-17 21:56:55 +0300

    Третья. квадраты в обе стороны

---
+++ sem2/example3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+Console.WriteLine("Ведите первое число");
+int numb1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ведите второе число");
+int numb2 = int.Parse(Console.ReadLine()!);
+
+if (numb1 * numb1 == numb2 || numb2 * numb2 == numb1)
+  {
+  Console.WriteLine("да");
+  }
+else 
+{
+    Console.WriteLine("нет");
+}
