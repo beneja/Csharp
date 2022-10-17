da36f495a640cd8b4c161741c9b804dbffb6687d
AlekseiPanov <panov.a.a60@gmail.com>
2022-10-17 21:06:23 +0300

    совместная работа

---
+++ sem2/sam4/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Задача 10: Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа.*/
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int second = num / 100;
+int third = num % 10;
+int result = second * 10 + third;
+
+Console.Write($"{num}, {result}");
