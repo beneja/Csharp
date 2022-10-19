790a44b35d66b1069f2ee0f36d43469236101e52
AlekseiPanov <panov.a.a60@gmail.com>
2022-10-19 22:37:42 +0300

    example2 var1

---
+++ sem2/Homework/example2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая выводит третью цифру (справа налево)
+//заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите трёхзначное число: ");
+int numb = int.Parse(Console.ReadLine()!);
+if (numb > 99)
+{
+    while (numb > 999)
+    {
+        numb = (numb - (numb%10))/10;
+        Console.WriteLine(numb);
+    }
+    int third = numb - numb%100;
+    Console.WriteLine($"третья спраца цифра {third/100}");
+}
+else Console.WriteLine("Третьей цифры нет");
