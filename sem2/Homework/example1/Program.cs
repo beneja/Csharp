33ff44cbdce64d93a889a6e433a4d07757ff9065
AlekseiPanov <panov.a.a60@gmail.com>
2022-10-19 21:48:02 +0300

    example1

---
+++ sem2/Homework/example1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num * num / num > 99 && num * num / num < 1000)
+{
+    int second = (num % 100 - num % 10) / 10;
+    Console.WriteLine($"Вторая цифра {second}");
+
+}
