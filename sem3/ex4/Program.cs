94b1b052419d2581a060b082b4f84f2bc61ccdb1
AlekseiPanov <panov.a.a60@gmail.com>
2022-10-20 22:22:19 +0300

    seminar 3

---
+++ sem3/ex4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+using System;
+class MyProgram
+{
+    static void getPows(int N){
+        for (int i=1;i<=N;i++) Console.Write((i*i).ToString()+((i!=N)?", ":""));
+    }
+ 
+    static void Main()
+    {
+        Console.Write("Введите N:");
+        int n = int.Parse(Console.ReadLine());
+        
+        getPows(n);
+    }
+}
