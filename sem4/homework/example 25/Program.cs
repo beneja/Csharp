d99fb36d4a0d4bbea27f6b4cdd99e4160c6d5485
AlekseiPanov <panov.a.a60@gmail.com>
2022-11-09 22:34:30 +0300

    Возведение в степень и сумма разрядов числа

---
+++ sem4/homework/example 25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int Exponentiation(int number, int degree)
+{
+    int Result = number;
+    for (int count = 1; count < degree; count++)
+    {
+        Result *= number;
+    }
+    return Result;
+}
+Console.Write("Введите число: ");
+int Number = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень: ");
+int Degree = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Exponentiation(Number, Degree));
---
+++ sem4/homework/example 27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите функцию и запустите ее, которая
+// принимает на вход число и выдаёт сумму цифр в числе.
+int SumOfDigits(int number)
+{
+    int result = 0;
+    while(number != 0)
+    {
+        
+        result += (number%10);
+        number /= 10; 
+        Console.WriteLine($"{number}, {result}");
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int Number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumOfDigits(Number));
