02ca68c503b79234d314419716c7f91283b7c6d9
AlekseiPanov <panov.a.a60@gmail.com>
2022-11-28 18:57:13 +0300

    ex 8. done

--- sem8/homework/example 60/Program.cs
+++ sem8/homework/example 60/Program.cs
@@ -5,23 +5,39 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
-int[,,] Get3DArray(int m, int n, int l)
+int[,,] Get3DArray(int m, int n, int l, int[] array)
 {
     int[,,] result = new int[m, n, l];
-    int value = 10;
+    int index = 0;
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
             for (int k = 0; k < l; k++)
             {
-                result[i, j, k] = value;
-                value++; 
+                result[i, j, k] = array[index];
+                index++;
             }
         }
     }
     return result;
 }
+int[] NonRepeatingRandom(int minValue, int maxValue, int size)
+    {
+        int[] randomArray = new int[size];
+        for(int i = 0; i < size; i++)
+        {
+            randomArray[i] = new Random().Next(minValue, maxValue + 1);
+            for(int j = 0; j < i; j++)
+           {
+               if(randomArray[i] == randomArray[j])
+               {
+                i--;
+               }
+           }
+        } return randomArray;
+    }
+
 void PrintArray(int[,,] inArray)
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
@@ -36,9 +52,15 @@ void PrintArray(int[,,] inArray)
         }
     }
 }
-int rows = 2;
-int cols = 2;
-int dim3 = 2;
-int[,,] array = Get3DArray(rows, cols, dim3, 10, 99);
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int dim3 = int.Parse(Console.ReadLine()!);
+
+int[] randomArray = NonRepeatingRandom(10, 99, rows*cols*dim3);
+Console.WriteLine(String.Join(", ", randomArray));
+int[,,] array = Get3DArray(rows, cols, dim3, randomArray);
 PrintArray(array);
 
--- sem8/homework/example 62/Program.cs
+++ sem8/homework/example 62/Program.cs
@@ -4,14 +4,4 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
-void PrintArray(int[,] inArray)
-{
-    for (int i = 0; i < inArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < inArray.GetLength(1); j++)
-        {
-            Console.Write($"{inArray[i, j]} \t");
-        }
-        Console.WriteLine();
-    }
-}
+
